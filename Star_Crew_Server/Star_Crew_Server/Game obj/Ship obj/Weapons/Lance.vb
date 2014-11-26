646d40c16b9cd83f6975e1bd74e7eb35a62cb8d0
Dynisious <daniel@bechaz.com>
2014-11-26 22:28:09 +1100

    Hot Fix
    *Fixed a small bug that would crash the Server if you fired a missile
    without a target to track.

--- Star_Crew_Server/Star_Crew_Server/Game obj/Ship obj/Weapons/Lance.vb
+++ Star_Crew_Server/Star_Crew_Server/Game obj/Ship obj/Weapons/Lance.vb
@@ -6,7 +6,11 @@
     End Sub
 
     Protected Overrides Sub Fire_Weapon()
-        Server.Combat.adding.Add(New Missile(Parent.target, 0.6, 4200, 200, Parent.Speed, 21, Parent.Direction, 20, Parent.X, Parent.Y, Parent.Allegiance))
+        If Parent.target IsNot Nothing Then 'There is a target to shoot
+            Server.Combat.adding.Add(New Missile(Parent.target, 0.6, 4200, 200, Parent.Speed, 21, Parent.Direction, 20, Parent.X, Parent.Y, Parent.Allegiance))
+        Else
+            Ammunition.Current += 1 'The ammunition gets replenished by one
+        End If
     End Sub
 
 End Class
